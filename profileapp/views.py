7d69e0ac417c179d47d7c40f08d64c46397a2995
Rehan Habib <rehan.habib777@gmail.com>
2023-11-14 19:50:55 +0500

    Add get_object_or_404 exception in profileapp/views.py

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseServerError
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseServerError, Http404
 from django.contrib.auth.models import User
+from .models import Profile
 from django.contrib import messages
 from .forms import ProfileForm
 from django.views import View
@@ -18,17 +18,21 @@ class ProfileView(View):
         return render(request, self.template_name, {'form': form, 'full_name':full_name})
 
     def post(self, request, *args, **kwargs):
-        user_instance = request.user.profile
-        if request.method == "POST":
-            form = self.form_class(request.POST,request.FILES, instance=user_instance)
-            try:
+        user_profile = request.user.profile
+        try:
+            user_instance = get_object_or_404(User, profile = user_profile)
+            print(user_instance)
+            if request.method == "POST":
+                form = self.form_class(request.POST,request.FILES, instance=user_instance)
+
                 if request.user.is_authenticated:
                     if form.is_valid():
                         form.save()
                         return redirect('index')
                     return render(request, self.template_name, {'form': form})
-            except Exception as e:
-                return HttpResponseServerError(f"An error occurred: {e} You should login first")    
-        form = self.form_class(instance=request.user.profile)   
+                return HttpResponseServerError("You should login first") 
+        except Exception as e:
+            return Http404(f"An error occurred: {e} ")    
+        form = self.form_class(instance=user_instance)   
         full_name = user_instance.full_name 
         return render(request, self.template_name, {'form': form, 'full_name':full_name})
