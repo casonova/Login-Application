864282d219a1853dfddaf94e62496fbdd6fc83d3
Rehan Habib <rehan.habib777@gmail.com>
2023-11-15 19:24:02 +0500

    Update Reademe

--- profileapp/views.py
+++ profileapp/views.py
@@ -12,27 +12,30 @@ class ProfileView(View):
     form_class = ProfileForm
 
     def get(self, request, *args, **kwargs):
-        user_instance = request.user.profile
+        try:
+            user_instance = get_object_or_404(Profile, user= request.user)
+        except user_instance.DoesNotExist:
+            raise Http404("Profile of User does not exist")     
+            
         full_name = user_instance.full_name
         form = self.form_class(instance=user_instance)
         return render(request, self.template_name, {'form': form, 'full_name':full_name})
 
     def post(self, request, *args, **kwargs):
-        user_profile = request.user.profile
         try:
-            user_instance = get_object_or_404(User, profile = user_profile)
-            print(user_instance)
-            if request.method == "POST":
-                form = self.form_class(request.POST,request.FILES, instance=user_instance)
+            user_instance = get_object_or_404(Profile, user= request.user)
+        except user_instance.DoesNotExist:
+            raise Http404("Profile of User doesnot exist")     
+            
+        if request.method == "POST":
+            form = self.form_class(request.POST,request.FILES, instance=user_instance)
 
-                if request.user.is_authenticated:
-                    if form.is_valid():
-                        form.save()
-                        return redirect('index')
-                    return render(request, self.template_name, {'form': form})
-                return HttpResponseServerError("You should login first") 
-        except Exception as e:
-            return Http404(f"An error occurred: {e} ")    
+            if request.user.is_authenticated:
+                if form.is_valid():
+                    form.save()
+                    return redirect('index')
+                return render(request, self.template_name, {'form': form})
+            return HttpResponseServerError("You should login first")    
         form = self.form_class(instance=user_instance)   
         full_name = user_instance.full_name 
         return render(request, self.template_name, {'form': form, 'full_name':full_name})
